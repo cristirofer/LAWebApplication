456c08e30df31d0886b5e51dcb71b02679a2a320
Cristina Rodríguez <132570636+cristirofer@users.noreply.github.com>
2024-04-29 13:07:13 +0200

    gxf

--- obj/Debug/net8.0/LAWebApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/LAWebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LAWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ff6a62a7c5621253c7b2a013108f73806847fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33366c86b0287199f8b4486e17b76c664044f6d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("LAWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LAWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
